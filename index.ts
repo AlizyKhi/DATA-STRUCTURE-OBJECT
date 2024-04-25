88f7e2614674b2c23f962d6f3138680ed421c980
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-04-25 11:08:44 +0500

    Data Structure OBJECT Practice

---
+++ index.ts
@@ -0,0 +1,52 @@
+let studentOneName:string = "ABc"
+let studentOneAge:number = 111
+let studentTwoName:string = "aBC"
+let studentTwoAge:number = 222
+
+
+let studentOne: { 
+     name:string,
+     age:number
+}={
+    name:"ABc",
+    age:111
+}
+console.log (studentOne)
+
+
+let studentTwo: {
+    name:string,
+    age:number
+}={
+    name:"aBC",
+    age:222
+}
+console.log (studentTwo)
+
+
+let relativesOneName:string = "sadia"
+let relativesOneChildrenCount:number = 3
+let relativesTwoName:string = "akbar"
+let relstivesTwoChildrenCount:number = 5
+
+
+
+let relativesOne: {
+    name:string,
+    count:number
+}={
+    name:"sadia",
+    count:3
+}
+console.log (relativesOne)
+
+let relativesTwo: {
+    name:string,
+    count:number
+}={
+    name:"akbar",
+    count:5
+}
+console.log (relativesTwo)
+
+
